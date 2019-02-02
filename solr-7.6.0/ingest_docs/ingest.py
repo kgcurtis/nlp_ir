be69f2f790973ec8a6bef05359e4304a407ab59e
kgcurtis <kcurtis26518@gmail.com>
2019-02-02 12:09:49 -0600

    Create new clean core

--- solr-7.6.0/ingest_docs/ingest.py
+++ solr-7.6.0/ingest_docs/ingest.py
@@ -1,7 +1,7 @@
 import json
 import pysolr
 
-solr = pysolr.Solr('http://localhost:8983/solr/my_collection/', timeout=10)
+solr = pysolr.Solr('http://localhost:8983/solr/my_core/', timeout=10)
 
 tmdb_data = open('tmdb.json').read()
 tmdb = json.loads(tmdb_data)

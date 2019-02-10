ef167fdfbeaf70af4dc0105a1d5894e36a4e6a82
Hadi Matny <matny.hadi@gmail.com>
2019-02-10 12:59:12 -0600

    added field for actors, characters

--- solr-7.6.0/ingest_docs/ingest.py
+++ solr-7.6.0/ingest_docs/ingest.py
@@ -7,8 +7,21 @@ tmdb_data = open('tmdb.json').read()
 tmdb = json.loads(tmdb_data)
 
 tmdbArr = []
+name_array = []
+char_array = []
+
 for id in tmdb:
-    tmdbArr.append(tmdb[id])
+    #tmdbArr.append(tmdb[id])
+    movie = tmdb[id]
+    name_array = []
+    char_array = []
+    for item in tmdb[id]['cast']:
+        name_array.append(item['name'])
+        char_array.append(item['character'])
+    
+    movie['actors'] = name_array
+    movie['characters'] = char_array[0:5]
+    tmdbArr.append(movie)
 
 solr.add(tmdbArr)
 solr.commit()
